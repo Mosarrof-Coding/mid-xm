ba742ab925f619026006207c9a464cff70f9f5ae
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-08-29 22:34:27 +0600

    added

---
+++ dist/script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".btn2",
+    prevEl: ".btn",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
